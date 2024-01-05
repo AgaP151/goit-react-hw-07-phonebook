6e7d8e85bfb038fed0cc96f90f70d826618d77fa
AgaP151 <piotrowska.agnieszka1987@gmail.com>
2024-01-06 00:28:33 +0100

    added basename to the router

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { contactsSlice } from './contactsSlice';
 import { filterSlice } from './filterSlice';
 
